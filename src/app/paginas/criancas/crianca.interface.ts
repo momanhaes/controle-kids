2e2eb03b09dd3d415a6b6128fbe7911505cd31a3
Matheus Manhães <momanhaes@gmail.com>
2019-08-05 20:08:21 -0300

    Adiciona página de formulário, home e ajuste no css

---
+++ src/app/paginas/criancas/crianca.interface.ts
@@ -0,0 +1,14 @@
+export interface Crianca {
+    nome: string;
+    responsavel: string;
+    telefone: string;
+    horaInicio: string;
+    tempo: string;
+    horaFim: string;
+    valor: number;
+    // status: number;
+    pagamento: boolean;
+    // valorPago: number;
+    // troco: number;
+}
+
